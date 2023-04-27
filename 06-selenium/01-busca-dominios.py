f87351e96c957b8a03e9d11a0aab935ff0a8a1aa
daianeklein <daiane.klein22@gmail.com>
2023-04-27 10:50:29 -0300

    add list

--- 06-selenium/01-busca-dominios.py
+++ 06-selenium/01-busca-dominios.py
@@ -6,9 +6,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-
-
-
 import time
 
 print('Iniciando...')
@@ -17,32 +14,46 @@ print('Iniciando...')
 path = '/Users/daianeklein/Documents/DS/python-rpa/python-rpa/06-selenium/chrome-web-driver/'
 service = Service(executable_path=path + "chromedriver")
 driver = webdriver.Chrome(service=service)
-driver.get("https://registro.br/")
-
-time.sleep(2)
 
 # Looking for the domain availability
-pesquisa = driver.find_element(By.ID, "is-avail-field")
-pesquisa.clear()
-pesquisa.send_keys('roboscompython.com.br')
-time.sleep(2)
+dominios = ['roboscompython.com.br', 'www.uol.com.br', 'www.pythonabc123.com.br', 'www.udemy.com']
 
-# Save the current result element before submitting the form
-old_result = driver.find_element(By.XPATH, "//p[contains(@class, 'font-3') and contains(@class, 'is-avail-response')]/span/strong")
+for dominio in dominios:
+    driver.get("https://registro.br/")
+    time.sleep(2)
 
-# Submit the form
-pesquisa.submit()
+    pesquisa = driver.find_element(By.ID, "is-avail-field")
+    pesquisa.clear()
+    pesquisa.send_keys(dominio)
+    time.sleep(2)
 
-# Wait for the old result to become stale
-wait = WebDriverWait(driver, 10)
-wait.until(EC.staleness_of(old_result))
+    # Save the current result element before submitting the form
+    result_locator = (By.XPATH, "//p[contains(@class, 'font-3') and contains(@class, 'is-avail-response')]/span/strong")
+    old_result = driver.find_element(*result_locator)
+    old_text = old_result.text
 
-# Wait for the new result to become visible
-results = wait.until(EC.visibility_of_element_located((By.XPATH, "//p[contains(@class, 'font-3') and contains(@class, 'is-avail-response')]/span/strong")))
+    # Submit the form
+    pesquisa.submit()
+    time.sleep(3)
 
-# Getting the results
-text_result = results.text
-print(text_result)
+    try:
+        # Wait for the result to change
+        wait = WebDriverWait(driver, 20) # Increase timeout to 20 seconds
+        wait.until(EC.text_to_be_present_in_element(result_locator, old_text), message=f"Result didn't change for domain {dominio}")
+
+        # Wait for the new result to become visible
+        results = driver.find_element(*result_locator)
+        time.sleep(3)
+
+        # Getting the results
+        text_result = results.text
+        print(dominio, text_result)
+        time.sleep(3)
+
+    except TimeoutException as e:
+        print(f"Timeout while processing domain: {dominio}")
+        print(str(e))
+        continue
 
 time.sleep(5)
 driver.close()
